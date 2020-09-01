dc7f8cebbea262d7e7e0ea708342ceded764edab
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2020-09-01 10:58:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 window.onload = function(){
+	var username = document.getElementById("username"),
+		password = document.getElementById("password");
+	var showPass = document.getElementById("show-hide");
 	var loginBtn = document.getElementById("login");
 	loginBtn.addEventListener("click",submitLogin);
 	function submitLogin(){
 		loginBtn.classList.add("active");
+	}
+
+	password.addEventListener("keyup",function(){
+		if(password.value != "") showPass.style.display = "block";
+			else showPass.style.display = "none"
+	})
+
+	showPass.onclick = function(){
+		this.classList.toggle("hide-pass");
+		if(password.type == "password"){
+			password.type = "text"
+		}else password.type = "password"
 	}
 }
